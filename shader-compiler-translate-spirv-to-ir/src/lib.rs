5799390caa68e8e242d46b9bb47587e531c50b42
Jacob Lifshay <programmerjake@gmail.com>
2020-02-20 21:56:33 -0800

    switch tests to using external files

--- shader-compiler-translate-spirv-to-ir/src/lib.rs
+++ shader-compiler-translate-spirv-to-ir/src/lib.rs
@@ -216,6 +216,8 @@ impl<'g> TranslatedSPIRVShader<'g> {
 #[cfg(test)]
 mod tests {
     use core::fmt;
+    use shader_compiler_ir::assert_ir_matches_file;
+    use spirv_parser::convert_bytes_to_words;
 
     struct PrintOutput;
 
@@ -228,107 +230,11 @@ mod tests {
 
     #[test]
     fn trivial_test() {
-        let spirv_code = &[
-            0x0723_0203, // ..#.
-            0x0001_0000, // ....
-            0x0008_0007, // ....
-            0x0000_0008, // ....
-            0x0000_0000, // ....
-            0x0002_0011, // ....
-            0x0000_0001, // ....
-            0x0006_000B, // ....
-            0x0000_0002, // ....
-            0x4C53_4C47, // GLSL
-            0x6474_732E, // .std
-            0x3035_342E, // .450
-            0x0000_0000, // ....
-            0x0003_000E, // ....
-            0x0000_0000, // ....
-            0x0000_0001, // ....
-            0x0005_000F, // ....
-            0x0000_0005, // ....
-            0x0000_0005, // ....
-            0x6E69_616D, // main
-            0x0000_0000, // ....
-            0x0006_0010, // ....
-            0x0000_0005, // ....
-            0x0000_0011, // ....
-            0x0000_0001, // ....
-            0x0000_0001, // ....
-            0x0000_0001, // ....
-            0x0004_0007, // ....
-            0x0000_0001, // ....
-            0x6964_7473, // stdi
-            0x0000_006E, // n...
-            0x002E_0003, // ....
-            0x0000_0002, // ....
-            0x0000_01C2, // ....
-            0x0000_0001, // ....
-            0x4F20_2F2F, // // O
-            0x646F_4D70, // pMod
-            0x5065_6C75, // uleP
-            0x6563_6F72, // roce
-            0x6465_7373, // ssed
-            0x696C_6320, //  cli
-            0x2074_6E65, // ent
-            0x6B6C_7576, // vulk
-            0x3031_6E61, // an10
-            0x2F2F_0A30, // 0.//
-            0x4D70_4F20, //  OpM
-            0x6C75_646F, // odul
-            0x6F72_5065, // ePro
-            0x7373_6563, // cess
-            0x7420_6465, // ed t
-            0x6567_7261, // arge
-            0x6E65_2D74, // t-en
-            0x7576_2076, // v vu
-            0x6E61_6B6C, // lkan
-            0x0A30_2E31, // 1.0.
-            0x4F20_2F2F, // // O
-            0x646F_4D70, // pMod
-            0x5065_6C75, // uleP
-            0x6563_6F72, // roce
-            0x6465_7373, // ssed
-            0x746E_6520, //  ent
-            0x702D_7972, // ry-p
-            0x746E_696F, // oint
-            0x6961_6D20, //  mai
-            0x6C23_0A6E, // n.#l
-            0x2065_6E69, // ine
-            0x7623_0A31, // 1.#v
-            0x6973_7265, // ersi
-            0x3420_6E6F, // on 4
-            0x0A0A_3035, // 50..
-            0x6469_6F76, // void
-            0x6961_6D20, //  mai
-            0x2029_286E, // n()
-            0x2020_0A7B, // {.
-            0x6572_2020, //   re
-            0x6E72_7574, // turn
-            0x007D_0A3B, // ;.}.
-            0x0004_0005, // ....
-            0x0000_0005, // ....
-            0x6E69_616D, // main
-            0x0000_0000, // ....
-            0x0002_0013, // ....
-            0x0000_0003, // ....
-            0x0003_0021, // !...
-            0x0000_0004, // ....
-            0x0000_0003, // ....
-            0x0005_0036, // 6...
-            0x0000_0003, // ....
-            0x0000_0005, // ....
-            0x0000_0000, // ....
-            0x0000_0004, // ....
-            0x0002_00F8, // ....
-            0x0000_0006, // ....
-            0x0004_0008, // ....
-            0x0000_0001, // ....
-            0x0000_0004, // ....
-            0x0000_0000, // ....
-            0x0001_00FD, // ....
-            0x0001_0038, // 8...
-        ];
+        let spirv_code = convert_bytes_to_words(include_bytes!(concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            "/test_data/trivial_test.spv"
+        )))
+        .unwrap();
         let global_state = shader_compiler_ir::GlobalState::new();
         let translated_shader = crate::TranslatedSPIRVShader::new(
             &global_state,
@@ -337,291 +243,27 @@ mod tests {
             &mut PrintOutput,
             "main",
             spirv_parser::ExecutionModelGLCompute.into(),
-            spirv_code,
+            &spirv_code,
         )
         .map_err(|e| e.to_string())
         .unwrap();
-        let expected_text = concat!(
-            "module {\n",
-            "    target_properties {\n",
-            "        data_pointer_underlying_type: i64,\n",
-            "        function_pointer_underlying_type: i64,\n",
-            "    }\n",
-            "    built_in_inputs_block {\n",
-            "        -> built_in_inputs_block : data_ptr;\n",
-            "        size: fixed 0x0;\n",
-            "        align: 0x1;\n",
-            "    }\n",
-            "    user_inputs_block {\n",
-            "        -> user_inputs_block : data_ptr;\n",
-            "        size: fixed 0x0;\n",
-            "        align: 0x1;\n",
-            "    }\n",
-            "    built_in_outputs_block {\n",
-            "        -> built_in_outputs_block : data_ptr;\n",
-            "        size: fixed 0x0;\n",
-            "        align: 0x1;\n",
-            "    }\n",
-            "    user_outputs_block {\n",
-            "        -> user_outputs_block : data_ptr;\n",
-            "        size: fixed 0x0;\n",
-            "        align: 0x1;\n",
-            "    }\n",
-            "    invocation_global_variables {\n",
-            "    }\n",
-            "    fn main[] -> [] {\n",
-            "        hints {\n",
-            "            inlining_hint: none,\n",
-            "            side_effects: normal,\n",
-            "        }\n",
-            "        {\n",
-            "        }\n",
-            "        id_6 {\n",
-            "            break id_6[] @ \"stdin\":4:0;\n",
-            "        }\n",
-            "    }\n",
-            "    entry_point: main;\n",
-            "}",
+        assert_ir_matches_file!(
+            translated_shader.module,
+            concat!(
+                env!("CARGO_MANIFEST_DIR"),
+                "/test_data/trivial_test.kazan-ir"
+            )
         );
-        let text = translated_shader.module.to_string();
-        println!("translated module:\n{}", text);
-        assert_eq!(expected_text, text);
     }
 
     #[test]
     #[ignore] // FIXME: parsing not completely implemented yet; remove #[ignore] once implemented
     fn simple_test() {
-        let spirv_code = &[
-            0x0723_0203,
-            0x0001_0000,
-            0x0008_0001,
-            0x0000_002C,
-            0x0000_0000,
-            0x0002_0011,
-            0x0000_0001,
-            0x0002_0011,
-            0x0000_000B,
-            0x0006_000B,
-            0x0000_0001,
-            0x4C53_4C47,
-            0x6474_732E,
-            0x3035_342E,
-            0x0000_0000,
-            0x0003_000E,
-            0x0000_0000,
-            0x0000_0001,
-            0x0007_000F,
-            0x0000_0000,
-            0x0000_0004,
-            0x6E69_616D,
-            0x0000_0000,
-            0x0000_000A,
-            0x0000_000F,
-            0x0005_0048,
-            0x0000_0008,
-            0x0000_0000,
-            0x0000_000B,
-            0x0000_0000,
-            0x0003_0047,
-            0x0000_0008,
-            0x0000_0002,
-            0x0004_0047,
-            0x0000_000F,
-            0x0000_001E,
-            0x0000_0000,
-            0x0002_0013,
-            0x0000_0002,
-            0x0003_0021,
-            0x0000_0003,
-            0x0000_0002,
-            0x0003_0016,
-            0x0000_0006,
-            0x0000_0020,
-            0x0004_0017,
-            0x0000_0007,
-            0x0000_0006,
-            0x0000_0004,
-            0x0003_001E,
-            0x0000_0008,
-            0x0000_0007,
-            0x0004_0020,
-            0x0000_0009,
-            0x0000_0003,
-            0x0000_0008,
-            0x0004_003B,
-            0x0000_0009,
-            0x0000_000A,
-            0x0000_0003,
-            0x0004_0015,
-            0x0000_000B,
-            0x0000_0020,
-            0x0000_0001,
-            0x0004_002B,
-            0x0000_000B,
-            0x0000_000C,
-            0x0000_0000,
-            0x0004_0017,
-            0x0000_000D,
-            0x0000_0006,
-            0x0000_0003,
-            0x0004_0020,
-            0x0000_000E,
-            0x0000_0001,
-            0x0000_000D,
-            0x0004_003B,
-            0x0000_000E,
-            0x0000_000F,
-            0x0000_0001,
-            0x0004_002B,
-            0x0000_0006,
-            0x0000_0011,
-            0x3F80_0000,
-            0x0004_0020,
-            0x0000_0016,
-            0x0000_0003,
-            0x0000_0007,
-            0x0004_0020,
-            0x0000_0018,
-            0x0000_0007,
-            0x0000_000B,
-            0x0004_0015,
-            0x0000_001A,
-            0x0000_0020,
-            0x0000_0000,
-            0x0004_002B,
-            0x0000_001A,
-            0x0000_001B,
-            0x0000_0002,
-            0x0004_0020,
-            0x0000_001C,
-            0x0000_0001,
-            0x0000_0006,
-            0x0004_0015,
-            0x0000_001F,
-            0x0000_0040,
-            0x0000_0000,
-            0x0004_002B,
-            0x0000_0006,
-            0x0000_0028,
-            0x0000_0000,
-            0x0004_002B,
-            0x0000_001A,
-            0x0000_0029,
-            0x0000_0000,
-            0x0004_0020,
-            0x0000_002A,
-            0x0000_0003,
-            0x0000_0006,
-            0x0005_0036,
-            0x0000_0002,
-            0x0000_0004,
-            0x0000_0000,
-            0x0000_0003,
-            0x0002_00F8,
-            0x0000_0005,
-            0x0004_003B,
-            0x0000_0018,
-            0x0000_0019,
-            0x0000_0007,
-            0x0004_003D,
-            0x0000_000D,
-            0x0000_0010,
-            0x0000_000F,
-            0x0005_0051,
-            0x0000_0006,
-            0x0000_0012,
-            0x0000_0010,
-            0x0000_0000,
-            0x0005_0051,
-            0x0000_0006,
-            0x0000_0013,
-            0x0000_0010,
-            0x0000_0001,
-            0x0005_0051,
-            0x0000_0006,
-            0x0000_0014,
-            0x0000_0010,
-            0x0000_0002,
-            0x0007_0050,
-            0x0000_0007,
-            0x0000_0015,
-            0x0000_0012,
-            0x0000_0013,
-            0x0000_0014,
-            0x0000_0011,
-            0x0005_0041,
-            0x0000_0016,
-            0x0000_0017,
-            0x0000_000A,
-            0x0000_000C,
-            0x0003_003E,
-            0x0000_0017,
-            0x0000_0015,
-            0x0005_0041,
-            0x0000_001C,
-            0x0000_001D,
-            0x0000_000F,
-            0x0000_001B,
-            0x0004_003D,
-            0x0000_0006,
-            0x0000_001E,
-            0x0000_001D,
-            0x0004_006D,
-            0x0000_001F,
-            0x0000_0020,
-            0x0000_001E,
-            0x0004_0071,
-            0x0000_001A,
-            0x0000_0021,
-            0x0000_0020,
-            0x0004_007C,
-            0x0000_000B,
-            0x0000_0022,
-            0x0000_0021,
-            0x0003_003E,
-            0x0000_0019,
-            0x0000_0022,
-            0x0004_003D,
-            0x0000_000B,
-            0x0000_0023,
-            0x0000_0019,
-            0x0003_00F7,
-            0x0000_0026,
-            0x0000_0000,
-            0x000C_00FB,
-            0x0000_0020,
-            0x0000_0025,
-            0x0000_0001,
-            0x0000_0000,
-            0x0000_0024,
-            0x0000_0002,
-            0x0000_0000,
-            0x0000_0024,
-            0x0000_0008,
-            0x0000_0000,
-            0x0000_0024,
-            0x0002_00F8,
-            0x0000_0025,
-            0x0006_0041,
-            0x0000_002A,
-            0x0000_002B,
-            0x0000_000A,
-            0x0000_000C,
-            0x0000_0029,
-            0x0003_003E,
-            0x0000_002B,
-            0x0000_0028,
-            0x0002_00F9,
-            0x0000_0026,
-            0x0002_00F8,
-            0x0000_0024,
-            0x0002_00F9,
-            0x0000_0026,
-            0x0002_00F8,
-            0x0000_0026,
-            0x0001_00FD,
-            0x0001_0038,
-        ];
+        let spirv_code = convert_bytes_to_words(include_bytes!(concat!(
+            env!("CARGO_MANIFEST_DIR"),
+            "/test_data/simple_test.spv"
+        )))
+        .unwrap();
         let global_state = shader_compiler_ir::GlobalState::new();
         let translated_shader = crate::TranslatedSPIRVShader::new(
             &global_state,
@@ -630,13 +272,16 @@ mod tests {
             &mut PrintOutput,
             "main",
             spirv_parser::ExecutionModelVertex.into(),
-            spirv_code,
+            &spirv_code,
         )
         .map_err(|e| e.to_string())
         .unwrap();
-        let expected_text = concat!("");
-        let text = translated_shader.module.to_string();
-        println!("translated module:\n{}", text);
-        assert_eq!(expected_text, text);
+        assert_ir_matches_file!(
+            translated_shader.module,
+            concat!(
+                env!("CARGO_MANIFEST_DIR"),
+                "/test_data/simple_test.kazan-ir"
+            )
+        );
     }
 }
